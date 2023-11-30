da90811377885910bf2f06ba1ffee1f2ab71f76d
Thayna Santos <102188509+tainasays@users.noreply.github.com>
2023-11-30 23:44:40 +0000

    [feat]first commit da aula MA

---
+++ soma.py
@@ -0,0 +1,15 @@
+def soma(a: float, b:float = 40) -> float:
+    """ Sum two values.
+
+    Take two variables ad sum.
+
+    Args:
+        a (float): first number.
+        b (float, optional): second number. Defaults to 5.
+
+    Returns:
+        float: The sum of 'a' and 'b'.
+    """
+    numero = a + b
+   
+    return numero
